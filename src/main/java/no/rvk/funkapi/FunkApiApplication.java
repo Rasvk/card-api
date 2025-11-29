3279016d034e124d9fe0e9988df64a237b621e6c
Rasvk <Rasmusvedholmkrog@gmail.com>
2025-11-29 11:59:27 +0100

    Initial commit

---
+++ src/main/java/no/rvk/funkapi/FunkApiApplication.java
@@ -0,0 +1,13 @@
+package no.rvk.funkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/rvk/funkapi/FunkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.rvk.funkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
